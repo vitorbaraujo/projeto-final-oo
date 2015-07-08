4e117b5694c2f440b2237194cab0ed209f0f742f
Vítor Barbosa <vitornga15@gmail.com>
2015-07-08 01:27:27 -0300

    Adding restriction for non-admin users to see list of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
-  
+  before_filter :is_owner, only: [:show]
+
   def show
   	@user = User.all
   end
+
+  def is_owner
+  	if user_signed_in?
+    	if current_user.email != "admin@admin.org"
+        	redirect_to "/", notice: 'You have no permission to access this page.'      
+    	end
+    else
+    		redirect_to "/", notice: 'You have no permission to access this page.'      
+    end
+  end
+
 end
